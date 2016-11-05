59c497a87d6c884f5c947f6bc83a09bc0c295fdd
Andrew Bell <fiixed@gmail.com>
2016-11-05 11:07:28 -0500

    added Photon Network, Photon Voice, VRTK and Hand Painted Lanscape asset.  Added Network Manager.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    public byte Version = 1;
+
+    /// <summary>if we don't want to connect in Start(), we have to "remember" if we called ConnectUsingSettings()</summary>
+    private bool ConnectInUpdate = true;
+
+
+    public virtual void Start() {
+         PhotonNetwork.ConnectUsingSettings(Version + "." + SceneManagerHelper.ActiveSceneBuildIndex);
+        
+    }
+
+    // below, we implement some callbacks of PUN
+    // you can find PUN's callbacks in the class PunBehaviour or in enum PhotonNetworkingMessage
+
+
+    public virtual void OnConnectedToMaster() {
+        Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnJoinedLobby() {
+        Debug.Log("OnJoinedLobby(). This client is connected and does get a room-list, which gets stored as PhotonNetwork.GetRoomList(). This script now calls: PhotonNetwork.JoinRandomRoom();");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnPhotonRandomJoinFailed() {
+        Debug.Log("OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4 }, null);
+    }
+
+    // the following methods are implemented to give you some context. re-implement them as needed.
+
+    public virtual void OnFailedToConnectToPhoton(DisconnectCause cause) {
+        Debug.LogError("Cause: " + cause);
+    }
+
+    public void OnJoinedRoom() {
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
+    }
+}
