fb29f23c62875330a66c11be49e9a4d54785b08e
Andrew Bell <fiixed@gmail.com>
2016-11-06 15:25:25 -0600

    moved all objects to ForestScene

---
+++ Assets/Scripts/ChangeOwners.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using VRTK;
+
+public class ChangeOwners : Photon.MonoBehaviour {
+
+    public VRTK.VRTK_InteractableObject toss;
+
+	// Use this for initialization
+	void Start () {
+        toss.InteractableObjectGrabbed += Toss_InteractableObjectGrabbed;
+        StartCoroutine(Pop());
+	}
+
+    private IEnumerator Pop() {
+        yield return new WaitForSeconds(10f);
+        PhotonNetwork.Destroy(this.gameObject);
+    }
+
+    private void Toss_InteractableObjectGrabbed(object sender, InteractableObjectEventArgs e) {
+        toss.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player.ID);
+    }
+
+  
+}
